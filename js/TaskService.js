9dfa9dca988a91c9e808660e3589e1dae47cde8e
sarah <sarahphan2314@gmail.com>
2021-11-22 21:35:46 +0700

    Do the rest

--- js/TaskService.js
+++ js/TaskService.js
@@ -5,4 +5,42 @@ export default class TaskService{
             method: "GET",
         });
     };
+
+    deleteTaskApi = (id) => {
+        return axios({
+            url: `https://6183cae591d76c00172d1b53.mockapi.io/api/task/${id}`,
+            method: "DELETE",
+        });
+    };
+
+    addTaskApi = (taskObj) => {
+        return axios({
+            url: "https://6183cae591d76c00172d1b53.mockapi.io/api/task",
+            method: "POST",
+            data: taskObj,
+        });
+    };
+
+    getTaskById = (id) => {
+        return axios({
+            url: `https://6183cae591d76c00172d1b53.mockapi.io/api/task/${id}`,
+            method: "GET",
+        });
+    };
+
+    updateTaskById = (id, taskObj) => {
+        return axios({
+            url: `https://6183cae591d76c00172d1b53.mockapi.io/api/task/${id}`,
+            method: "PUT",
+            data: taskObj,
+        });
+    };
+
+    updateTaskStatus = (id, taskObj) => {
+        return axios({
+            url: `https://6183cae591d76c00172d1b53.mockapi.io/api/task/${id}`,
+            method: "PUT",
+            data: taskObj,
+        });
+    };
 }
--- js/main.js
+++ js/main.js
@@ -1,19 +1,34 @@
 import Task from "./Task.js";
 import TaskService from "./TaskService.js";
+import Validation from "./Validation.js";
 
 const service = new TaskService();
+const validation = new Validation();
 
 const getEleID = (id) => (document.getElementById(id));
 const getEleClass = (className) => (document.getElementsByClassName(className));
 
+let isLoading = false;
+const checkLoading = (isLoading) => {
+    if (isLoading){
+        getEleID("myBody").classList.add("loading");
+        getEleClass("card")[0].style.display = "none";
+    }
+    if(!isLoading){
+        getEleID("myBody").classList.remove("loading");
+        getEleClass("card")[0].style.display = "block";
+    }
+}
+
 const fetchData = () => {
+    isLoading = true;
+    checkLoading(isLoading);
     service.getListTaskApi()
         .then((result) => {
             // console.log(result);
             renderData(result.data);
-            // if(result.data.status == "todo"){
-            //     renderDataToDo(result.data);
-            // }
+            isLoading = false;
+            checkLoading(isLoading);
         })
         .catch((error) => {
             alert(error);
@@ -32,10 +47,14 @@ const renderData = (data) => {
                 <li>
                     <span>${item.textTask}</span>
                     <div class="buttons">
-                        <button class="remove">
+                        <button class="remove" onclick="deleteTask(${item.id})">
                             <i class="fa fa-trash-alt"></i>
                         </button>
-                        <button class="complete">
+                        <button class="edit" onclick="getTask(${item.id})">
+                            <i class="fa fa-edit"></i>
+                        </button>
+                        <button class="complete" 
+                        onclick="changeState(${item.id}, '${item.textTask}')">
                             <i class="far fa-check-circle"></i>
                             <i class="fas fa-check-circle"></i>
                       </button>
@@ -50,8 +69,11 @@ const renderData = (data) => {
                     <li>
                     <span>${item.textTask}</span>
                     <div class="buttons">
-                        <button class="remove">
+                        <button class="remove" onclick="deleteTask(${item.id})">
                             <i class="fa fa-trash-alt"></i>
+                        </button>
+                        <button class="edit" onclick="getTask(${item.id})">
+                            <i class="fa fa-edit"></i>
                         </button>
                         <button class="complete">
                             <i class="far fa-check-circle"></i>
@@ -66,3 +88,103 @@ const renderData = (data) => {
     })
 
 }
+
+const deleteTask = (id) => {
+    service.deleteTaskApi(id)
+        .then((result) => {
+            // console.log(result);
+            fetchData();
+            alert("Delete success!");
+            
+        })
+        .catch((error) => {
+            alert(error);
+        })
+}
+window.deleteTask = deleteTask;
+
+const addTask = () => {
+    const textTask = getEleID("newTask").value;
+    
+    let isValid = true;
+
+    isValid &= validation.checkEmpty(textTask, "Task is required!", "announce")
+
+    if(isValid){
+        const taskObj = new Task("", textTask, "todo");
+        // console.log(taskObj);
+    
+        service.addTaskApi(taskObj)
+            .then((result) => {
+                // console.log(result);
+                fetchData();
+                alert("Add success!");
+                location.reload();
+            })
+            .catch((error) => {
+                alert(error);
+            });
+    }
+}
+window.addTask = addTask;
+
+const getTask = (id) => {
+    const buttonUpdate =`
+        <input
+              id="newTask"
+              type="text"
+              placeholder="Enter an activity..."
+        />
+        <button class="update" onclick="updateTask(${id})">
+            <i class="fa fa-check"></i>
+        </button>
+    `;
+    getEleClass("card__add")[0].innerHTML = buttonUpdate;
+    service.getTaskById(id)
+        .then((result) => {
+            // console.log(result);
+            getEleID("newTask").value = result.data.textTask;
+         
+        })
+        .catch((error) => {
+            alert(error);
+        })
+}
+window.getTask = getTask;
+
+const updateTask = (id) => {
+    const textTask = getEleID("newTask").value;
+
+    const taskObj = new Task("", textTask, "todo");
+    // console.log(taskObj);
+
+    service.updateTaskById(id, taskObj)
+        .then((result) => {
+            // console.log(result);
+            fetchData();
+            alert("Update success!");   
+            location.reload();
+        })
+        .catch((error) => {
+            alert(error);
+        });
+}
+window.updateTask = updateTask;
+
+const changeState = (id, textTask) => {
+    const status = "completed";
+
+    const taskObj = new Task(id, textTask, status);
+    // console.log(taskObj);
+
+    service.updateTaskStatus(id, taskObj)
+    .then((result) => {
+        // console.log(result);
+        fetchData();
+    })
+    .catch((error) => {
+        alert(error);
+    })
+}
+window.changeState = changeState;
+
